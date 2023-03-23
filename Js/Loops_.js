eed8f6b16360ece600b606ee7283cee18c803665
raviacts035 <ravindrainturi035@gmail.com>
2023-03-23 11:27:42 +0530

    Playing with Loops and Conditional statements

---
+++ Js/Loops_.js
@@ -0,0 +1,13 @@
+// let count=10;
+
+// while (count>0){
+//     console.log("You have ",count," candys more...");
+//     count--;
+// }
+// console.log("All candys are over")
+let stng='';
+for (let a=0;a<10;a++){
+    stng+="#";
+    console.log(stng);
+}
+console.log("pattern End")
