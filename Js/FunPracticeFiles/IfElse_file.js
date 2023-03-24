6d4f94311daa85ed210950d853058cbd564b92b7
raviacts035 <ravindrainturi035@gmail.com>
2023-03-24 08:51:27 +0530

    javascript arrays-2 video practice

---
+++ Js/FunPracticeFiles/IfElse_file.js
@@ -0,0 +1,17 @@
+let age=21;
+
+if (age<18){
+    console.log("Child, don't eat too much chacolates")
+}
+else if (age<30){
+    console.log("Adult, learn, work and chill")
+}
+else if (age<60){
+    console.log("You are a MAN/WOMEN")
+}
+else if (age>=60){
+    console.log("Ceniour Citizen, you should go for a GOA Trip")
+}
+else {
+    console.log("invalid age")
+}
---
+++ Js/FunPracticeFiles/arrFiles.js
@@ -0,0 +1,29 @@
+// Array methods in javascript.......
+let fru=["apple","banana","cherry","mango"];
+
+console.log(fru)
+console.log(fru.length)
+console.log(fru[2])
+fru[1]="grape";
+console.log(fru)
+
+// slice
+// console.log(fru.slice(0,2));
+// Splice - takes 3 parameters, - (from,delete numberof ele/Replace,Elements), - returns none
+// fru.splice(2,2,"item1","item2")
+// console.log(fru);
+
+// Concat - attachesh one array at the end of another array, while printing
+console.log(fru.concat([1,2,3,76]));
+
+// fill
+// include(7,6) - checks wheather 6th index value is 7 or not. returns Boolean
+// indexOf("apple") - Returns the index value of given element.
+// Array.isArray(fru) -- checks weather passed variable is of type ARRAY or not.
+// .join(" ") -- combines all the elements of array into a single string with  
+
+
+// 
+// Another way of creating array
+// let arr2 = new Array('name 1', 'name 2','name 3');
+// console.log(arr2);
